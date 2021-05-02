87090abd3891e5b81096c03f6b607ab630e6bc17
Katya Litvinchuk <punkosergey1997@gmail.com>
2021-05-02 23:34:54 +0300

    Updates

--- backend/form.php
+++ backend/form.php
@@ -21,29 +21,26 @@ header('Content-type:application/json');
     // $mail->IsSMTP();
 
 
-    try {
-        //Recipients
-        $mail->setFrom('sergeypunko1997@gmail.com', 'Perfomante.io');
-        $mail->addAddress('punkosergey1997@gmail.com'); // Add a recipient
+    //Recipients
+    $mail->setFrom('sergeypunko1997@gmail.com', 'Perfomante.io');
+    $mail->addAddress('punkosergey1997@gmail.com'); // Add a recipient
 
 
-        // Content
-        $mail->isHTML(true); // Set email format to HTML
-        $mail->CharSet = 'UTF-8';
-        $mail->Subject = 'Perfomante.io — Video creatives project request';
-        $mail->Body    = 'Hi! We got your project request. We connect with you within next 24 hours.<br><br>Your email: <br><br>—<br>hi@perfomante.io<br>Perfomante.io - Boost your apps! Launch more video ads.';
-        $mail->AltBody = 'Hi! We got your project request. We connect with you within next 24 hours.<br><br>Your email: <br><br>—<br>hi@perfomante.io<br>Perfomante.io - Boost your apps! Launch more video ads.';
+    // Content
+    $mail->isHTML(true); // Set email format to HTML
+    $mail->CharSet = 'UTF-8';
+    $mail->Subject = 'Perfomante.io — Video creatives project request';
+    $mail->Body    = 'Hi! We got your project request. We connect with you within next 24 hours.<br><br>Your email: <br><br>—<br>hi@perfomante.io<br>Perfomante.io - Boost your apps! Launch more video ads.';
+    $mail->AltBody = 'Hi! We got your project request. We connect with you within next 24 hours.<br><br>Your email: <br><br>—<br>hi@perfomante.io<br>Perfomante.io - Boost your apps! Launch more video ads.';
 
-        $mail->send();
-
+    
+    if(!$mail->send()) {
         $result = 'Thank you, we got your request, check your email!';
         $data = ['success' => 'true', 'payload' => $result];
         echo json_encode($data);
-    } catch (Exception $e) {
-        $result = "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-        $data = ['success' => 'false', 'payload' => $result];
-        echo json_encode($data);
     }
-// endif;
+    $result = "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    $data = ['success' => 'false', 'payload' => $result];
+    echo json_encode($data);
 
 ?>
